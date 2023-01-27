82af2b1ca1950a199452d848eefcd6066a6ce8cf
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-01-27 14:50:32 -0500

    practicing React

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const name = "Truc";
+
+  const handleNameChange = () => {
+    const names = ["Bob", "Kevin", "Dave"];
+    const int = Math.floor(Math.random() * 2);
+    return names[int];
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to see changes.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello {handleNameChange()}!</p>
+        {/* curly bracets indicate js expression */}
+        {/* <p>{name}</p> */}
       </header>
     </div>
   );
