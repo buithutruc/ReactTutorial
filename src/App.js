b3881ea7f63e390715f599786fe65391e0614e84
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-01-28 18:54:15 -0500

    create external components & import them

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 
 function App() {
-  const name = "Truc";
-
-  const handleNameChange = () => {
-    const names = ["Bob", "Kevin", "Dave"];
-    const int = Math.floor(Math.random() * 2);
-    return names[int];
-  };
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello {handleNameChange()}!</p>
-        {/* curly bracets indicate js expression */}
-        {/* <p>{name}</p> */}
-      </header>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+//just type _rafce, then enter. Command + Shift + R doesn't work, for some reason
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Groceries List</h1>
+    </header>
+  );
+};
+
+export default Header;
