f9f71fd708dd90ca9d5a304b8e41fd7126055a9b
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-01-29 18:51:06 -0500

    applied styles to react components

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,12 @@
-//just type _rafce, then enter. Command + Shift + R doesn't work, for some reason
+//just type rafce, then enter. Command + Shift + R doesn't work, for some reason
 
 const Header = () => {
+  //   const headerStyle = {
+  //     backgroundColor: "royalblue",
+  //     color: "#fff",
+  //   };
   return (
+    // <header style={headerStyle}>
     <header>
       <h1>Groceries List</h1>
     </header>
